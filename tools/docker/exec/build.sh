c2163724a7a235802910b7b294676003651ad8d3
Olivier Chafik <ochafik@google.com>
2022-09-24 20:29:50 +0100

    Simplify tools/docker/exec/build.sh

--- tools/docker/exec/build.sh
+++ tools/docker/exec/build.sh
@@ -2,9 +2,28 @@
 set -euo pipefail
 cd "$(dirname "$0")/../../.."
 
-docker build -t v86:build-env -f tools/docker/exec/Dockerfile .
-docker run --rm -it -v $PWD:/v86 v86:build-env make all
+echo '
+  FROM alpine:3.14
+  RUN apk add --update curl clang make openjdk8 npm python3
+  RUN curl --proto "=https" --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y 
+  ENV PATH "/root/.cargo/bin:${PATH}"
+  RUN rustup target add wasm32-unknown-unknown
+  WORKDIR /v86
+' | docker build -t v86:build-env -f - .
 
-python3 -m http.server 8000 || \
-  docker run --rm -it -p 8000:8000 -v $PWD:/v86 python:3.9.6-alpine3.14 \
-    sh -c "cd v86 && python3 -m http.server 8000"
+docker run --rm -it -v $PWD:/v86 -p 8000:8000 v86:build-env \
+  sh -c "make -j all && make run"
+
+# Start a local server
+# Note: if python3 is installed, simply run:
+#   python3 -m http.server 8000
+# docker run --rm -it \
+#   -v $PWD:/v86 \
+#   v86:build-env \
+#   make run
+
+
+# docker run --rm -it -v $PWD:/v86 v86:build-env make -j all
+#  \
+# docker run --rm -it -p 8000:8000 -v $PWD:/v86 python:3.9.6-alpine3.14 \
+#     sh -c "cd v86 && python3 -m http.server 8000"
