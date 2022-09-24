d13c9662e07551858b8a13182506d07760a60ca9
Olivier Chafik <ochafik@google.com>
2022-09-24 17:59:20 +0100

    Incremental builds w/ tools/docker/exec/build.sh + serve without docker if possible

--- tools/docker/exec/build.sh
+++ tools/docker/exec/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
-cd "$(dirname "$0")"
-tar -cf - ../../../{*.html,*.js,*.css,gen,lib,src,bios,.cargo,Cargo.toml,Makefile,tools} | \
-    docker build -t v86:alpine-3.14 -f tools/docker/exec/Dockerfile -
+cd "$(dirname "$0")/../../.."
+
+docker build -t v86:build-env -f tools/docker/exec/Dockerfile .
+docker run --rm -it -v $PWD:/v86 v86:build-env make all
+
+python3 -m http.server 8000 || \
+  docker run --rm -it -p 8000:8000 -v $PWD:/v86 python:3.9.6-alpine3.14 \
+    sh -c "cd v86 && python3 -m http.server 8000"
